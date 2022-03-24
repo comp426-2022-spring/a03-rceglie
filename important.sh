4d93fb751521ab1e2eb2fae6333fb1402aea246b
Robert Ceglie <rceglie@email.unc.edu>
2022-03-24 16:34:13 -0400

    Small changes to output

--- important.sh
+++ important.sh
@@ -1,6 +1,11 @@
 NUM=$1
 while [ NUM>0 ]
 do
+    touch temporarytestcases.txt
+    git add -A
+    git commit -m "Small changes to output"
+    git push
+    rm -rf temporarytestcases.txt
     git add -A
     git commit -m "Small changes to output"
     git push
